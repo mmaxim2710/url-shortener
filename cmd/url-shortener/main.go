e6010f152eb60aba481cb94a8c96329e66fbdf85
m.zherlygin <m.zherlygin@arenadata.io>
2024-02-15 01:58:18 +0300

    init logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"fmt"
 	"github.com/mmaxim2710/url-shortener/internal/config"
+	"github.com/mmaxim2710/url-shortener/pkg/logger"
+	"log/slog"
 )
 
 func main() {
 	cfg := config.MustLoad()
+	fmt.Println(cfg.Env)
 
-	fmt.Println(cfg)
+	log := logger.SetupLogger(cfg.Env)
 
-	// logger
+	log.Info("starting URL shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 
 	// storage
 
---
+++ pkg/logger/logger.go
@@ -0,0 +1,50 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envStage = "stage"
+	envProd  = "prod"
+)
+
+func SetupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+				AddSource: false,
+				Level:     slog.LevelDebug,
+			}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+				AddSource: false,
+				Level:     slog.LevelDebug,
+			}),
+		)
+	case envStage:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+				AddSource: false,
+				Level:     slog.LevelDebug,
+			}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+				AddSource: false,
+				Level:     slog.LevelInfo,
+			}),
+		)
+	}
+
+	return log
+}
